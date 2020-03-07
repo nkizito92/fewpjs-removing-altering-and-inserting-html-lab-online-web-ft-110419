c3b92dd5f90dfee772f7380fb4a2f6dba5df0aa2
Kizito Njoku <nkizito57@gmail.com>
2020-03-07 22:49:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your code here!
 
 let newHeader = document.createElement('h1')
-
-document.body.appendChild(newHeader)
 newHeader.id = 'victory'
 newHeader.innerHTML = 'YOUR-NAME is the champion'
+document.body.appendChild(newHeader)
